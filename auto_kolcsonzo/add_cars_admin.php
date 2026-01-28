b02e9223660e8fff849a8c55db1de2d2697f2d48
Sigma-ifjusag <mezner.kilian2@gmail.com>
2026-01-28 12:19:56 +0100

    Add files via upload

--- auto_kolcsonzo/add_cars_admin.php
+++ auto_kolcsonzo/add_cars_admin.php
@@ -1,7 +1,27 @@
 <?php
 session_start();
+
 if (!isset($_SESSION['userid']) || $_SESSION['jogosultsag'] != 1) {
-    header("Location: login.php");
-    exit;
+    header("Location: logout.php");
+    exit();
 }
 ?>
+<script>
+function autoLogout() {
+    navigator.sendBeacon("logout.php");
+}
+
+// Kilépéskor, lap elhagyásakor
+window.addEventListener("unload", autoLogout);
+</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>keep it real king admin</h1>
+</body>
+</html>
--- auto_kolcsonzo/add_cars_user.php
+++ auto_kolcsonzo/add_cars_user.php
@@ -1,7 +1,34 @@
 <?php
 session_start();
-if (!isset($_SESSION['userid']) || $_SESSION['jogosultsag'] != 0) {
-    header("Location: login.php");
-    exit;
+
+// Ha nincs bejelentkezve → logout.php → index.php
+if (!isset($_SESSION['userid'])) {
+    header("Location: logout.php");
+    exit();
+}
+
+// (OPCIONÁLIS) Ha csak normál user léphet be (nem admin)
+if ($_SESSION['jogosultsag'] != 0) {
+    header("Location: logout.php");
+    exit();
 }
 ?>
+<script>
+function autoLogout() {
+    navigator.sendBeacon("logout.php");
+}
+
+// Kilépéskor, lap elhagyásakor
+window.addEventListener("unload", autoLogout);
+</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>keep it real king user</h1>
+</body>
+</html>
